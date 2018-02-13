8bae327ec98796b10b06ee91c30f4d322895b437
Mritunjay Kumar <mritunjay.kumar@rackspace.com>
2018-02-12 18:52:36 -0800

    Added debugging statement to log what input being sent for query.

--- src/main/java/com/rackspacecloud/blueflood/metricsqueryservice/service/MetricsService.java
+++ src/main/java/com/rackspacecloud/blueflood/metricsqueryservice/service/MetricsService.java
@@ -5,6 +5,8 @@ import com.rackspacecloud.blueflood.metricsqueryservice.model.MetaData;
 import com.rackspacecloud.blueflood.metricsqueryservice.model.MetricsResponse;
 import com.rackspacecloud.blueflood.metricsqueryservice.model.MetricsValue;
 import com.rackspacecloud.blueflood.metricsqueryservice.repository.IMetricsFullRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +18,19 @@ public class MetricsService implements IMetricsService {
     @Autowired
     IMetricsFullRepository metricsFullRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetricsService.class);
+
     @Override
     public MetricsResponse getMetricFullResolution(String key, long from, long to){
-        Iterable<MetricsFullModel> records = metricsFullRepository.findByKeyAndCollectionTimeBetween(key, from, to);
+        Iterable<MetricsFullModel> records = null;
+        try {
+            LOGGER.debug("Before call of 'getMetricFullResolution': key:[{}],from:[{}],to:[{}]", key, from, to);
+            records = metricsFullRepository.findByKeyAndCollectionTimeBetween(key, from, to);
+            LOGGER.debug("After call of 'getMetricFullResolution'");
+        }
+        catch(Exception ex){
+            LOGGER.error("'findByKeyAndCollectionTimeBetween' failed with exception message:{}", ex.getMessage());
+        }
 
         MetricsResponse response = new MetricsResponse();
         List<MetricsValue> metricsValues = new ArrayList<>();
